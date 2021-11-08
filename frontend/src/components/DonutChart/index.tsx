d1f34d8ed75b8db1b698650020639b746513e96b
Cleber A Machado <cammacpoa@gmail.com>
2021-11-08 00:22:34 -0300

    BarChart integration

--- frontend/src/components/DonutChart/index.tsx
+++ frontend/src/components/DonutChart/index.tsx
@@ -1,5 +1,6 @@
 
 import axios from 'axios';
+import { useEffect, useState } from 'react';
 import Chart from 'react-apexcharts'
 import { SaleSum } from 'types/sale';
 import { BASE_URL } from 'utils/request';
@@ -11,39 +12,37 @@ type ChartData = {
 }
 
 const DonutChart = () => {
-    // forma errada
-    let chartData : ChartData = { labels: [] , series: []};
-    
-    // forma errada
-    axios.get(`${BASE_URL}/sales/amount-by-seller`)
-         .then(response => {
-            
-            const data = response.data as SaleSum[];
-            const myLabels = data.map(x => x.sellerName);
-            const mySeries = data.map(x => x.sum);
-            
-            chartData = {labels:myLabels , series: mySeries}
-            console.log(chartData);
 
-         });
+    const [chartData, setChatData] = useState<ChartData>({ labels: [], series: [] });
+
+    useEffect(() => {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+            .then(response => {
+                const data = response.data as SaleSum[];
+                const myLabels = data.map(x => x.sellerName);
+                const mySeries = data.map(x => x.sum);
+
+                setChatData({ labels: myLabels, series: mySeries });
+            });
+    }, []);
 
     //const mockData = {
     //    series: [477138, 499928, 444867, 220426, 473088],
     //    labels: ['Anakin', 'Barry Allen', 'Kal-El', 'Logan', 'Padmé']
     // }
-    
+
     const options = {
         legend: {
             show: true
         }
     }
-    
+
     return (
-        <Chart  
-           options={{...options,labels:chartData.labels}}
-           series={chartData.series}
-           type="donut"
-           height="240"
+        <Chart
+            options={{ ...options, labels: chartData.labels }}
+            series={chartData.series}
+            type="donut"
+            height="240"
         />
     );
 }
